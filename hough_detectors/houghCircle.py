fcd5f39a30f18779943117e21b72fae77cdceb97
omarcartera <c.omargamal@gmail.com>
2018-04-14 16:00:48 +0200

    hough line and circle detection added

---
+++ hough_detectors/houghCircle.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as color
+from skimage import filter
+import time
+
+
+def houghCircle(image):
+    Ny, Nx = image.shape
+    R_max = np.round(np.sqrt(Ny**2 + Nx**2))
+
+    R = 13
+
+    R_interval = np.arange(1, R_max)
+    thetas = np.deg2rad(np.arange(360))
+
+    # computing sin and cosine
+    cosine = np.cos(thetas)
+    sine   = np.sin(thetas)
+
+    accumulator = np.zeros((Ny, Nx, R))
+
+    # Now Start Accumulation
+    for col in range(Nx):
+        for row in range(Ny):
+            if (image[row][col]):
+                for i in range(360):
+                    for r in range(1, R):
+                        a = int(round(row - r * cosine[i]))
+                        b = int(round(col - r * sine[i]))
+
+                        try:
+                            accumulator[a, b, r] += 1
+
+                        except IndexError:
+                            pass
+
+    return accumulator, Nx, Ny
+
+
+def detectCircles(image, accumulator, threshold, Nx, Ny):
+    detectedCircles = np.where(accumulator >= threshold * accumulator.max())
+
+    ys = detectedCircles[0]
+    xs = detectedCircles[1]
+    rs = detectedCircles[2]
+
+
+    plotCircles(image, ys, xs, rs, Nx, Ny)
+        
+
+def plotCircles(image, ys, xs, rs, Nx, Ny):
+    fig = plt.figure('Circles')
+
+    plt.imshow(image)
+
+    circle = []
+
+    for i in range(len(ys)):
+        circle.append(plt.Circle((xs[i], ys[i]), rs[i], color=(1,0,0), fill=True))
+        fig.add_subplot(111).add_artist(circle[-1])
+
+    plt.xlim(0, Nx)
+    plt.ylim(Ny, 0)
+
+    plt.savefig('houghCircle.png')
+
+
+    
+if __name__ == '__main__':
+    ###################
+    start_time = time.time()
+
+    # Load the image
+    image = plt.imread('images/coins.jpg')
+
+    try:
+        # Get value Channel (intensity)
+        hsvImage = color.rgb_to_hsv(image)
+        valImage = hsvImage[...,2]
+
+    except ValueError:
+        valImage = image
+
+    # Edge detection (canny)
+    edgeImage = filter.canny(valImage,sigma=1.4, low_threshold=40, high_threshold=150)    
+    
+    # Show original image
+    plt.figure('Original Image')
+    plt.imshow(image)
+    plt.set_cmap('gray')
+    
+    # Show edge image
+    plt.figure('Edge Image')
+    plt.imshow(edgeImage)
+    plt.set_cmap('gray')
+    
+    # build accumulator    
+    accumulator, Nx, Ny = houghCircle(edgeImage)
+
+    # Detect and superimpose lines on original image
+    detectCircles(image, accumulator, 0.5, Nx, Ny)
+    
+
+    print("--- %0.3f seconds ---" % (time.time() - start_time))
+    ####################
+
+    plt.show()
---
+++ hough_detectors/houghLine.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import filter
+import matplotlib.colors as color
+import time
+
+
+def houghLine(image, Nx, Ny):
+    # Max diatance is diagonal one 
+    maxdist = int(np.round((Nx*Nx + Ny*Ny)**0.5)) 
+
+    # Range of radius, a line of 2*maxdist samples
+    rs = np.linspace(-maxdist, maxdist, 2 * maxdist)
+
+    # Theta in range from -90 to 90 degrees
+    thetas = np.deg2rad(np.arange(-90, 90, 1))
+
+    
+    # computing sin and cosine
+    cosine = np.cos(thetas)
+    sine   = np.sin(thetas)
+
+
+    # initialize accumulator array to zeros
+    accumulator = np.zeros((2 * maxdist, len(thetas)))
+
+    # Now Start Accumulation
+    for col in range(Nx):
+        for row in range(Ny):
+            if (image[row][col]):
+                for k in range(len(thetas)):
+                    r = col * cosine[k] + row * sine[k]
+                    accumulator[int(r) + maxdist, k] += 1
+
+    return accumulator, thetas, rs
+
+
+def detectLines(image, accumulator, threshold, rhos, thetas, Ny, Nx):
+    detectedLines = np.where(accumulator >= threshold * accumulator.max())
+
+    detectedRadius = rhos[detectedLines[0]]
+    detectedThetas = thetas[detectedLines[1]]
+
+    # Now plot detected lines in image
+    plotLines(image, detectedRadius, detectedThetas, Ny, Nx)
+
+
+def plotLines(image, detectedRadius, detectedThetas, Ny, Nx):
+    plt.figure('Lines')
+    plt.imshow(image)
+
+    x = np.linspace(0, Nx)
+
+    cosine = np.cos(detectedThetas)
+    sine = np.sin(detectedThetas)
+
+    cotan = cosine/sine
+    ratio = detectedRadius/sine
+
+    for i in range(len(detectedRadius)):
+        # if thete is not 0
+        if (detectedThetas[i]):
+            plt.plot(x, (-x * cotan[i]) + ratio[i])
+
+        # if theta is 0
+        else:
+            plt.axvline(detectedRadius[i])
+
+    plt.xlim(0, Nx)
+    plt.ylim(Ny, 0)
+
+
+    plt.savefig('houghLine.png')
+
+   
+
+
+if __name__ == '__main__':
+    ###################
+    start_time = time.time()
+
+    # Load the image
+    image = plt.imread('images/Lines.jpg')
+
+    Ny, Nx, _ = image.shape
+
+    # Get value Channel (intensity)
+    hsvImage = color.rgb_to_hsv(image)
+    valImage = hsvImage[...,2]
+    
+    # Detect edges using canny 
+    edgeImage = filter.canny(valImage, sigma = 1.4, low_threshold = 40, high_threshold = 150)
+
+    # Show original image
+    plt.figure('Original Image')
+    plt.imshow(image)
+    plt.set_cmap('gray')
+    
+    # Show edge image
+    plt.figure('Edge Image')
+    plt.imshow(edgeImage)
+    plt.set_cmap('gray')
+        
+    # build accumulator    
+    accumulator, thetas, rhos = houghLine(edgeImage, Nx, Ny)
+    
+
+
+    # Visualize hough space
+    plt.figure('Hough Space', figsize = (5,5))
+    plt.imshow(accumulator)
+    plt.set_cmap('gray')
+    
+    # Detect and superimpose lines on original image
+    detectLines(image, accumulator, 0.6, rhos, thetas, Ny, Nx)
+    
+    print("--- %0.3f seconds ---" % (time.time() - start_time))
+    ####################
+
+    plt.show()
